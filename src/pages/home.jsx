76d8166a0cd9e172521e58c2d0b9ac51f0bcf897
tesseract5702 <rudramagharia5555@gmail.com>
2023-04-29 17:53:32 +0530

    added api

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -9,7 +9,7 @@ export default function Home() {
   const [loading,setLoading] = useState(true);
 
   const getModel = async ()=>{
-    axios.get('http://localhost:3000/api/getModels')
+    axios.get('https://master--meek-pasca-6be50e.netlify.app/api/getModels')
     .then((res)=>{setModel(res.data)
                   setLoading(false)
     })
